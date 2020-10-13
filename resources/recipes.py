e85cbb6872c20f9dee0f84afcdee5d077e284f42
Joanna Nowakowska <joanna.k.nowakowska@gmail.com>
2020-10-13 10:37:28 +0200

    Adding the external api to a post method of /recipes

--- resources/recipes.py
+++ resources/recipes.py
@@ -1,4 +1,6 @@
 from flask_restful import Resource, reqparse
+import requests
+from external_api_key import EXTERNAL_API_KEY
 
 request_parser = reqparse.RequestParser()
 request_parser.add_argument('ingredients',
@@ -16,4 +18,34 @@ request_parser.add_argument('dish',
 class Recipes(Resource):
     def post(self):
         users_data = request_parser.parse_args()
-        return users_data
+
+        if users_data['dish']:
+            dish = f"&q={users_data['dish']}"
+
+        ingredients = '&i=' + users_data['ingredients'].replace(", ", '%2C')
+
+        page_no = '?p=1'
+
+        print(ingredients)
+
+        url = "https://recipe-puppy.p.rapidapi.com/"
+
+        final_endpoint = url + page_no + ingredients + dish
+
+        headers = {
+            'x-rapidapi-host': "recipe-puppy.p.rapidapi.com",
+            'x-rapidapi-key': EXTERNAL_API_KEY
+        }
+
+        response = requests.get(final_endpoint, headers=headers)
+
+        res = response.json()
+
+        list_of_results = res['results']
+        n = 1
+        for result in list_of_results:
+            print(f'''{n} - {result['title']}
+            Ingredients: {result['ingredients']},
+            Link to the recipe: {result['href']}
+            ''')
+            n += 1
